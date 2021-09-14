6fde8dc08d8a6897ce0d52da97d9b3d3e88a0910
Innocent607 <68567585+Innocent607@users.noreply.github.com>
2021-09-14 15:03:22 +0200

    check it out Gaone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import speech_recognition as sr
 import pyttsx3
-import pywhatkit
+#import pywhatkit
 import datetime
 import wikipedia
 import pyjokes
@@ -8,17 +8,26 @@ import pyjokes
 print("in")
 
 listener = sr.Recognizer()
-engine = pyttsx3.init()
+engine = pyttsx3.init() #'sapi5'
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
 
 
-def talk(text):
+def speak(text):
     engine.say(text)
     engine.runAndWait()
 
 
 def take_command():
+    with sr.Microphone() as source:
+        print('listening...')
+        voice = listener.listen(source)
+        command = listener.recognize_google(voice)
+        command = command.lower()
+        if 'alexa' in command:
+            command = command.replace('alexa', '')
+            print(command)
+    """
     try:
         with sr.Microphone() as source:
             print('listening...')
@@ -28,35 +37,36 @@ def take_command():
             if 'alexa' in command:
                 command = command.replace('alexa', '')
                 print(command)
-    except:
+    except  :
         pass
+    """
     return command
 
 
-def run_alexa():
+def run():
     command = take_command()
     print(command)
     if 'play' in command:
         song = command.replace('play', '')
         talk('playing ' + song)
-        pywhatkit.playonyt(song)
+        #pywhatkit.playonyt(song)
     elif 'time' in command:
         time = datetime.datetime.now().strftime('%I:%M %p')
-        talk('Current time is ' + time)
+        speak('Current time is ' + time)
     elif 'who the heck is' in command:
         person = command.replace('who the heck is', '')
         info = wikipedia.summary(person, 1)
         print(info)
-        talk(info)
+        speak(info)
     elif 'date' in command:
-        talk('sorry, I have a headache')
+        speak('sorry, I have a headache')
     elif 'are you single' in command:
-        talk('I am in a relationship with wifi')
+        speak('I am in a relationship with wifi')
     elif 'joke' in command:
-        talk(pyjokes.get_joke())
+        speak(pyjokes.get_joke())
     else:
-        talk('Please say the command again.')
+        speak('Please say the command again.')
 
 
 while True:
-    run_alexa()
+    run()
